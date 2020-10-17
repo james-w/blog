e0344ce77860163353ef79a04ead871ace541782
James Westby <james@jameswestby.net>
2020-10-17 12:35:14 +0100

    Remove the prefix

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://jameswestby.net/weblog'
-RELATIVE_URLS = False
+SITEURL = 'https://jameswestby.net/'
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
